370c47d2d20b62de3207e41c6c38a5d2a52d853e
Justin Rhodes <justinr@tri.be>
2025-09-30 10:58:58 -0500

    feat: add pre-commit hook and update ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,32 +1,39 @@
 import antfu from "@antfu/eslint-config";
 
-export default antfu({
-  typescript: true,
-  formatters: true,
-  stylistic: {
-    indent: 2,
-    semi: true,
-    quotes: "double",
+export default antfu(
+  {
+    typescript: true,
+    formatters: true,
+    stylistic: {
+      indent: 2,
+      semi: true,
+      quotes: "double",
+    },
+    ignores: ["drizzle", ".husky", "node_modules", "dist", "coverage"],
   },
-  ignores: [
-    "drizzle",
-  ],
-}, {
-  rules: {
-    "ts/no-redeclare": "off",
-    "node/file-extension-in-import": ["error", "always"],
-    "ts/consistent-type-definitions": ["error", "type"],
-    "no-console": ["warn"],
-    "antfu/no-top-level-await": ["off"],
-    "node/prefer-global/process": ["off"],
-    "node/no-process-env": ["error"],
-    "perfectionist/sort-imports": ["error", {
-      tsconfigRootDir: ".",
-    }],
-    "unicorn/filename-case": ["error", {
-      case: "kebabCase",
-      ignore: ["README.md"],
-    }],
-    "test/prefer-lowercase-title": ["off"],
+  {
+    rules: {
+      "ts/no-redeclare": "off",
+      "node/file-extension-in-import": ["error", "always"],
+      "ts/consistent-type-definitions": ["error", "type"],
+      "no-console": ["warn"],
+      "antfu/no-top-level-await": ["off"],
+      "node/prefer-global/process": ["off"],
+      "node/no-process-env": ["error"],
+      "perfectionist/sort-imports": [
+        "error",
+        {
+          tsconfigRootDir: ".",
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          case: "kebabCase",
+          ignore: ["README.md"],
+        },
+      ],
+      "test/prefer-lowercase-title": ["off"],
+    },
   },
-});
+);
